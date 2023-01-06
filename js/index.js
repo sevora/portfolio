3621fc83f33daa7737c10a3023386292e327149e
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2023-01-06 19:23:56 +0800

    Prepare MapWalkerRenderer The idea is to use some sort of pathfinding algorithm to trace around the circuit board images, revealing the circuit slowly while a gradient is below it.

--- js/index.js
+++ js/index.js
@@ -1,92 +1,9 @@
-import MoveGenerator from "./MoveGenerator";
+import MapWalkerRenderer from "./MapWalkerRenderer.js";
 
-// elements refer to the elements that should be scattered
-let elements = document.querySelectorAll(".scatter-parent");
-let generators = []; // has all the generators
-let queue = [];      // has the generators in queue for queueing the updates
+const canvas = document.querySelector("canvas");
+let x = new MapWalkerRenderer("../images/background/pattern-original-large.png", canvas, {
+  walkerSize: 100,
+  walkerSpeed: 30
+});
 
-function main() {
-    let particlesConfigPath = `assets/particlesjs-config${ window.innerWidth > 600 ? '' : '-mobile'}.json`;
-    particlesJS.load('particles', particlesConfigPath);
-
-    // first we generate and scatter all the moving text
-    for (let index = 0; index < elements.length; ++index) {
-        generators.push( new MoveGenerator(elements[index]) );
-        generators[index].scatterOnParent();
-    }
-
-    // add an event listener for the scrolling
-    document.body.addEventListener('scroll', function() { 
-      updateGeneratorsOnView(); 
-    });
-
-    // start the animation after a few milliseconds
-    setTimeout(function() {
-      updateGeneratorsOnView(); 
-    }, 1500);
-}
-
-/**
- * determines whether given element is in viewport
- * @param {HTMLElement} element DOM element
- * @returns boolean
- */
-function isElementInViewport(element) {
-    let rectangle = element.getBoundingClientRect();
-
-    return (
-        rectangle.top >= 0 &&
-        rectangle.left >= 0 &&
-        rectangle.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
-        rectangle.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-/**
- * this is specifically used as a callback for the queue
- * @param {MoveGenerator} generator a MoveGenerator instance
- * @returns null
- */
-function queueCallback(generator) {
-    queue.splice(0, 1);
-    if (queue.length > 0) {
-      queue[0].updateUntilDone(queueCallback);
-    }
-    return null;
-}
-
-/**
- * this updates the queue depending on if the elements with generators
- * are on view.
- */
-function updateGeneratorsOnView() {
-    // this is used so that the queue code isn't run again
-    // unless the queue is empty (refer to last line in function)
-    let runUpdate = queue.length == 0;
-
-    for (let index = 0; index < generators.length; ++index) {
-        let generator = generators[index];
-
-        // optimization here with the order of conditions
-        // checking if done is the fastest, checking if in queue prevents bloat in queue,
-        // and finally checking if in viewport last
-        if (!generator.isDone() && !queue.includes(generator) && isElementInViewport(generator.element)) {
-            queue.push(generator);
-        }
-    }
-  
-    // this one only runs the queue code if necessary
-    if (queue.length > 0 && runUpdate) {
-        queue[0].updateUntilDone(queueCallback);
-    }
-}
-
-// wait for the whole window to load
-window.onload = function() { 
-    // reload to fix weird getClientBoundingRect
-    if ( !sessionStorage.getItem('reload') ) { 
-        sessionStorage.setItem('reload', '1');
-        window.location.reload(); 
-    }
-    main(); 
-}
+x.loadAndStart();
