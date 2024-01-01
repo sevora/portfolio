72917fda511751317e1559b123d92022e55794cd
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-01-01 18:20:49 +0800

    Remove JS Interpolation Let CSS handle the animation. Much more efficient.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,18 @@
-import Interpolator from './interpolate';
-
 // these are the DOM elements necessary for this site
 const root: HTMLElement = document.querySelector(':root');
 const navigationLinks: HTMLAnchorElement[] = Array.from( document.querySelectorAll('#navigation-bar > a') );
-
-// these are custom objects to make this site functional
-const interpolator = new Interpolator(0, 0, 0, 10);
 
 /**
  * This is the entrypoint of the program
  * where everything is put together.
  */
 function main() {
-    // we set the interpolator's callback to set the value in the css accordingly
-    interpolator.callback = (value) => {
-        root.style.setProperty('--active-tab-index', String(value));
-    }
-
     // here, we hook up the navigation links with the interpolator
     navigationLinks.forEach( (anchor, index) => {
         anchor.addEventListener('click', () => {
             navigationLinks.forEach(anchor => anchor.classList.remove('active') );
             anchor.classList.add('active');
-            
-            interpolator.setRange(interpolator.endValue, index); // we set the range to where the interpolator was last and set the goal to the new index
-            interpolator.run();
-
+            root.style.setProperty('--active-tab-index', String(index) );
         });
     });
 }
