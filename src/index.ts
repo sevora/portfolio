a94799c510dd7a396ed499a076b65bfdb3ed8f30
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-01-02 22:30:41 +0800

    Design First Page

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
 // these are the DOM elements necessary for this site
 const root: HTMLElement = document.querySelector(':root');
-const navigationLinks: HTMLAnchorElement[] = Array.from( document.querySelectorAll('#navigation-bar > a') );
+const navigationLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('#navigation-bar > a');
 
 /**
  * This is the entrypoint of the program
  * where everything is put together.
  */
 function main() {
-    // here, we hook up the navigation links with the interpolator
-    navigationLinks.forEach( (anchor, index) => {
-        anchor.addEventListener('click', () => {
-            navigationLinks.forEach(anchor => anchor.classList.remove('active') );
-            anchor.classList.add('active');
+    // the url may contain a hash which indicates the content's id that should be shown
+    const contentId = window.location.hash;
+
+    // here, we hook up the navigation links with setting the tab index
+    navigationLinks.forEach( (link, index) => {
+        const { hash } = link;
+
+        link.addEventListener('click', (event) => {
+            const target = event.target as HTMLAnchorElement;
+           
+            // here we set the values to make the navigation bar look right
+            navigationLinks.forEach(link => link.classList.remove('active') );
+            target.classList.add('active');
             root.style.setProperty('--active-tab-index', String(index) );
+
+            // then of course we want to show the corresponding content
+            setContent(hash);
         });
+
+        // if the url has a hash we want to match the corresponding link
+        if (hash === contentId) link.click();
     });
+}
+
+/**
+ * Use this to set the content shown in the page.
+ * @param contentId corresponds to the id property of the content element (has a class called content).
+ */
+function setContent(contentId: string) {
+    const content = document.querySelector(`.content${contentId}`);
+    document.querySelectorAll('.content').forEach(content => content.classList.add('hidden') );
+    if (content) content.classList.remove('hidden');
 }
 
 // calls the main function when the page loads
