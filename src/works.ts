7e86e571a3e9ad780d1f55eeb203f4c549a5b76a
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-01-03 22:18:01 +0800

    Add Works

--- src/works.ts
+++ src/works.ts
@@ -1,39 +1,48 @@
-export default [
+const works: Highlight[] = [
     {
+        selector: '#aspire',
         title: 'Aspire',
         description: '',
         github: ''
     },
     {
+        selector: '#blog',
         title: 'Blog',
+        description: '',
         preview: 'https://blog.ralphlouisgopez.com',
         github: 'https://github.com/sevora/journey'
     },
     {
+        selector: '#cinelaya',
         title: 'Cinelaya',
         description: '',
         preview: 'https://workspace.cinelaya.com'
-   
     },
     {
+        selector: '#deebait',
         title: 'Deebait',
         description: '',
         github: ''
     },
     {
+        selector: '#fairu',
         title: 'Fairu',
         description: '',
         github: ''
     },
     {
+        selector: '#stmviz',
         title: 'stmviz',
         description: '',
         preview: 'https://visualizer.ralphlouisgopez.com',
         github: 'https://github.com/sevora/stmviz'
     },
     {
+        selector: '#story',
         title: 'Story',
         description: 'https://story.ralphlouisgopez.com',
         github: 'https://github.com/sevora/story'
-    },
-] as Highlight[];
+    }
+];
+
+export default works;
