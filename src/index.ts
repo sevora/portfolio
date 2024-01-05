0271dab4fe31f12fdc7234d121d2ba4383e1679e
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-01-05 20:16:39 +0800

    Add Works and Improve Setting of Content

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ const root: HTMLElement = document.querySelector(':root');
 const activeTab: HTMLDivElement = document.querySelector('#active-tab');
 const navigationLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('#navigation-bar > a');
 
+const hash2Index: { [hash: string]: number } = {};
+
 /**
  * This is the entrypoint of the program
  * where everything is put together.
  */
 function main() {
+    // remove smooth scrolling when js is enabled
+    window.document.body.style.setProperty('scroll-behavior', 'auto');
+
+    // when there's no hash, we want to use 'about'
+    if (!window.location.hash) window.location.hash = 'about';
+
     // the url may contain a hash which indicates the content's id that should be shown
     const contentId = window.location.hash;
 
@@ -18,33 +26,47 @@ function main() {
     // here, we hook up the navigation links with setting the tab index
     navigationLinks.forEach( (link, index) => {
         const { hash } = link;
+        hash2Index[hash] = index;
 
         link.addEventListener('click', (event) => {
+            // this is to prevent the automatic scroll to center but it also prevents changing URL
+            event.preventDefault();
             const target = event.target as HTMLAnchorElement;
-           
-            // here we set the values to make the navigation bar look right
-            navigationLinks.forEach(link => link.classList.remove('active') );
-            target.classList.add('active');
-            root.style.setProperty('--active-tab-index', String(index) );
 
-            // then of course we want to show the corresponding content
-            setContent(hash);
+            // we need to push the history in order to change the URL
+            history.pushState({ hash }, '', target.href);
+            displayContent(hash);
         });
 
-        // if the url has a hash we want to match the corresponding link
-        if (hash === contentId) link.click();
+        // if the url has a hash we want to set the corresponding content
+        if (hash === contentId) displayContent(hash);
     });
 }
 
 /**
- * Use this to set the content shown in the page.
- * @param contentId corresponds to the id property of the content element (has a class called content).
+ * Use this to set the navigation bar amd content shown in the page.
+ * @param contentId corresponds to the id property of the content element (also a hash of its 
+ * corresponding anchor tag and has a class called content).
  */
-function setContent(contentId: string) {
+function displayContent(contentId: string) {
+    // here we set the values to make the navigation bar look right
+    const index = hash2Index[contentId];
+    navigationLinks.forEach(link => link.classList.remove('active') );
+    navigationLinks[index].classList.add('active');
+    root.style.setProperty('--active-tab-index', String(index) );
+
+    // here we show the right content
     const content = document.querySelector(`.content${contentId}`);
     document.querySelectorAll('.content').forEach(content => content.classList.add('hidden') );
     if (content) content.classList.remove('hidden');
 }
+
+// this is called whenever the back button is pressed
+window.addEventListener('popstate', (event) => {
+    if (!event.state) return;
+    const hash: string = event.state.hash;
+    if (hash) displayContent(hash);
+});
 
 // calls the main function when the page loads
 window.addEventListener('load', main);
