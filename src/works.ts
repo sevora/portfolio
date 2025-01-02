9f3fc142ce92156ad95fa6a5842e745c97589fb8
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2025-01-02 14:29:48 +0800

    Update Works and Dependencies

--- src/works.ts
+++ src/works.ts
@@ -3,11 +3,6 @@
  * and so it is slightly optimized (?) and less code to write for searching.
  */
 const works: { [id: string]: WorkHighlight } = {
-    'aspire': {
-        title: 'ASPIRE',
-        description: 'A COVID risk-estimator web application. This was done mainly by a team in DLSU (De La Salle University) and I worked on optimizing or turning it into a custom single-page application. It is a web-application that computes the likelihood of viral spread (COVID) from given parameters such as location, activity, and so on.',
-        github: 'https://github.com/sevora/covid-risk-estimator'
-    },
     'blog': {
         title: 'Blog',
         description: 'A collection of stories I have working with tech. From nerdy talk about what I just made to my ideals and views, you can read about those here. Some entries are technical, some are just casual.',
@@ -18,21 +13,12 @@ const works: { [id: string]: WorkHighlight } = {
         title: 'bl0g.pro',
         description: 'This is an experimental blog-site project with special effects meant to add some immersion. Try it out. It\'s fun!',
         preview: 'https://bl0g.pro',
+        github: 'https://github.com/sevora/bl0g'
     },
     'cinelaya': {
         title: 'Cinelaya',
         description: 'A suite of tools to aid filmmakers bring their imagination into reality. This project, also a company, was founded by <a target="_blank" href="https://www.linkedin.com/in/gil-ponce-6704389">Gil Ponce</a>. I am the fullstack developer of the work-in-progress web application and is continuously adding features and optimizations into the site.',
-        preview: 'https://workspace.cinelaya.com'
-    },
-    'deebait': {
-        title: 'Deebait',
-        description: 'This is a website made for 1-to-1 anonymous chat but with a twist. It it not currently working or hosted anymore. Users select a preference regarding a topic and get matched up with someone who has answers opposite to theirs. For example, cat lovers will get matched with dog lovers and so on. The point is for people to have a debate.',
-        github: 'https://github.com/sevora/deebait'
-    },
-    'fairu': {
-        title: 'Fairu',
-        description: 'A file indexing web-application. Currently not hosted anywhere. Originally intended to index files in academia. It has a tagging function and a search feature. That\'s mostly about it. It also has a dashboard for admins to verify files.',
-        github: 'https://github.com/sevora/fairu'
+        preview: 'https://www.cinelaya.com'
     },
     'stmviz': {
         title: 'stmviz',
@@ -50,11 +36,6 @@ const works: { [id: string]: WorkHighlight } = {
         title: 'Simple Calculator',
         description: 'A simple calculator with the most basic of features made with Java 11 and JavaFX. What\'s good about this project is the fact that it is neatly organized and is easily extensible.',
         github: 'https://github.com/sevora/simple-calculator'
-    },
-    'ramble': {
-        title: 'Ramble',
-        description: 'A lightweight Twitter Clone structured in a simple, manageable, and extendable style. I made this primarily for a university project in our Computer Science Course and the goal is to be able to write a CRUD-application utilizing a SQL database management system.',
-        github: 'https://github.com/sevora/ramble'
     }
 };
 
