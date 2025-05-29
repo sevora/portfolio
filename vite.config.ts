bd3e04bdf721745875830c4d4105592ddad616d6
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2025-05-29 23:14:11 +0800

    Remove Unused Dependencies These were dependencies from the template used that aren't needed by this project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [ tailwindcss() ]
-})
+export default defineConfig({})
